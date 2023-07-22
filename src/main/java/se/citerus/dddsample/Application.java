2925d6d7c93fb5304db7ea4f9e9e4ddb224591da
Toshiaki Maki <makingx@gmail.com>
2023-07-22 10:33:50 +0900

    Polish

--- src/main/java/se/citerus/dddsample/Application.java
+++ src/main/java/se/citerus/dddsample/Application.java
@@ -2,14 +2,11 @@ package se.citerus.dddsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import se.citerus.dddsample.config.AppConfig;
-import se.citerus.dddsample.config.PathfinderConfig;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
