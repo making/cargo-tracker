423b8a6331d0c8d10753441d8e8df569e694b4f7
Toshiaki Maki <makingx@gmail.com>
2023-07-22 12:46:02 +0900

    Add access logger

--- src/main/java/se/citerus/dddsample/interfaces/InterfaceConfig.java
+++ src/main/java/se/citerus/dddsample/interfaces/InterfaceConfig.java
@@ -1,5 +1,6 @@
 package se.citerus.dddsample.interfaces;
 
+import am.ik.accesslogger.AccessLogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
@@ -9,15 +10,22 @@ import java.time.Duration;
 
 @Configuration
 public class InterfaceConfig {
+    @Bean
+    public AccessLogger accessLogger() {
+        return new AccessLogger(httpExchange -> {
+            final String uri = httpExchange.getRequest().getUri().getPath();
+            return uri != null && !(uri.equals("/readyz") || uri.equals("/livez") || uri.startsWith("/actuator"));
+        });
+    }
 
-	@Bean
-	public ThreadPoolTaskScheduler myScheduler(UploadDirectoryScanner scanner) {
-		ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
-		threadPoolTaskScheduler.setPoolSize(10);
-		threadPoolTaskScheduler.setThreadNamePrefix("ThreadPoolTaskScheduler");
-		threadPoolTaskScheduler.initialize();
-		threadPoolTaskScheduler.scheduleAtFixedRate(scanner, Duration.ofMillis(5000));
-		return threadPoolTaskScheduler;
-	}
+    @Bean
+    public ThreadPoolTaskScheduler myScheduler(UploadDirectoryScanner scanner) {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(10);
+        threadPoolTaskScheduler.setThreadNamePrefix("ThreadPoolTaskScheduler");
+        threadPoolTaskScheduler.initialize();
+        threadPoolTaskScheduler.scheduleAtFixedRate(scanner, Duration.ofMillis(5000));
+        return threadPoolTaskScheduler;
+    }
 
 }
